dc42e2cecbdcd166517ea05ee1317d3822f8bd33
JM <jmlallier@me.com>
2025-09-16 14:19:13 -0400

    Add a test page or easier ad debugging

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,43 +1,7 @@
 <template>
-    <div>
-        <button id="open-iframe" @click="openIframe">Click Me</button>
+    <div class="h-screen w-screen flex justify-center items-center">
+        <iframe 
+    class="w-256 h-full"
+    ref="iframe" src="/creations/dGhlc3dlZXRlc3RvY2Nhc2lvbi5jb206ODE=/interactive" frameborder="0" id="interactive-iframe"></iframe>
     </div>
-    <iframe ref="iframe" v-show="iframeVisible" allowfullscreen src="/creations/7/interactive" frameborder="0" id="interactive-iframe"></iframe>
 </template>
-
-<style>
-#iframe {
-    width: 100vw;
-    height: 100vh;
-    border: none;
-    display: hidden;
-}
-</style>
-
-<script setup>
-
-import { ref } from 'vue';
-
-const iframeVisible = ref(false);
-const iframe = ref(null);
-const isWindowMobile = ref(false);
-
-onMounted(() => {
-    isWindowMobile.value = window.innerWidth < 768;
-});
-
-function openIframe() {
-    iframeVisible.value = true;
-    if (isWindowMobile.value) {
-        iframe.value.requestFullscreen();
-    }
-}
-// do this the Vue way
-function closeIframe() {
-    iframeVisible.value = false;
-    if (isWindowMobile.value) {
-        document.exitFullscreen();
-    }
-}
-
-</script>
