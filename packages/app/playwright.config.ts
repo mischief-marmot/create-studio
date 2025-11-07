eb68597541be295777669e766f83b801ef597f64
JM <jmlallier@me.com>
2025-11-07 17:16:26 -0500

    let playwright decide workers

--- packages/app/playwright.config.ts
+++ packages/app/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Use 5 workers locally, 1 on CI for stability */
-  workers: process.env.CI ? 1 : 5,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
