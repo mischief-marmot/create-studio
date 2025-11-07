0fa55a0ed00447d60f4a0abae35faf471aee3569
JM <jmlallier@me.com>
2025-11-07 13:11:10 -0500

    use 5 playwrigh workers locally

--- packages/app/playwright.config.ts
+++ packages/app/playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Use 3 workers locally, 1 on CI for stability */
-  workers: process.env.CI ? 1 : 3,
+  /* Use 5 workers locally, 1 on CI for stability */
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
